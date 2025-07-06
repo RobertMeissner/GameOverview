82e370307db771c891ac8e07fa19754ad9e61205
Robert Meissner <50482279+RobertMeissner@users.noreply.github.com>
2025-07-06 08:20:24 +0200

    Added missign files

---
+++ worker/test-worker.js
@@ -0,0 +1,5 @@
+// Test worker entry point for integration tests
+// This is a simple JavaScript wrapper that imports the TypeScript worker
+
+import worker from './src/index.js'
+export default worker
