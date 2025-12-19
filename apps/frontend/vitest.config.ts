5db2fa2d8f3729ce4808ae8682411eb402ab15c8
Robert Meissner <50482279+RobertMeissner@users.noreply.github.com>
2025-12-19 08:42:11 +0100

    fix: add vitest confing

---
+++ apps/frontend/vitest.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import angular from '@analogjs/vite-plugin-angular';
+
+export default defineConfig({
+  plugins: [angular()],
+  test: {
+    globals: true,
+    setupFiles: ['src/test-setup.ts'],
+    environment: 'jsdom',
+    include: ['src/**/*.spec.ts'],
+  },
+});
